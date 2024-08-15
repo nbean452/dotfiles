80968cdacb246f2125a2503a19ab6953f9680c25
Nicholas Benedict <nicholasb1537@gmail.com>
2024-08-15 09:54:41 +0800

    add .vimrc, .wezterm.lua, and karabiner.json

---
+++ .wezterm.lua
@@ -0,0 +1,26 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+-- config.color_scheme = "AdventureTime"
+
+config.font = wezterm.font("JetbrainsMono Nerd Font", { bold = false, italic = false })
+config.font_size = 20
+
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
+
+config.hide_tab_bar_if_only_one_tab = true
+config.window_background_opacity = 1.0
+
+-- and finally, return the configuration to wezterm
+return config
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # dotfiles
+
+Here is a bunch of config files for my development environment. I use them to set up a new machine or to keep my current environment in sync.
+
