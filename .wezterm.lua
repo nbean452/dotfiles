ba17edba4f45df8e432d419683b2349a2f5150eb
Nicholas Benedict <nicholasb1537@gmail.com>
2024-08-17 01:24:21 +0800

    update `.wezterm.lua` and remove nvdash

--- .wezterm.lua
+++ .wezterm.lua
@@ -7,7 +7,7 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = "Everforest Dark (Gogh)"
+config.color_scheme = "One Dark (Gogh)"
 
 config.font = wezterm.font("JetbrainsMono Nerd Font", { bold = false, italic = false })
 config.font_size = 20
@@ -21,30 +21,6 @@ config.window_padding = {
 
 config.hide_tab_bar_if_only_one_tab = true
 config.window_background_opacity = 1.0
-
--- This is a helper function for folke/zen-mode.nvim plugin specifically for wezterm
-wezterm.on("user-var-changed", function(window, pane, name, value)
-	local overrides = window:get_config_overrides() or {}
-	if name == "ZEN_MODE" then
-		local incremental = value:find("+")
-		local number_value = tonumber(value)
-		if incremental ~= nil then
-			while number_value > 0 do
-				window:perform_action(wezterm.action.IncreaseFontSize, pane)
-				number_value = number_value - 1
-			end
-			overrides.enable_tab_bar = false
-		elseif number_value < 0 then
-			window:perform_action(wezterm.action.ResetFontSize, pane)
-			overrides.font_size = nil
-			overrides.enable_tab_bar = true
-		else
-			overrides.font_size = number_value
-			overrides.enable_tab_bar = false
-		end
-	end
-	window:set_config_overrides(overrides)
-end)
 
 -- and finally, return the configuration to wezterm
 return config
