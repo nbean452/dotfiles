8d2b7cb89b5ca60d4bd1d6b94a3a1035cbf94d1b
Nicholas Benedict <nicholasb1537@gmail.com>
2026-05-02 15:14:39 +0800

    feat(linux): wezterm.lua
    Use no ligature font instead of yes-ligature

--- linux/wezterm/wezterm.lua
+++ linux/wezterm/wezterm.lua
@@ -6,36 +6,55 @@ local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 
--- Color scheme, refer to https://wezterm.org/colorschemes/index.html
-config.color_scheme = "Monokai (dark) (terminal.sexy)"
+-- For example, changing the color scheme: https://wezterm.org/colorschemes/index.html
+config.color_scheme = "Night Owl (Gogh)"
 
 -- Font settings
--- cd ~/Downlods && wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/JetBrainsMono.zip
-config.font = wezterm.font("JetBrainsMono Nerd Font", { bold = true, italic = false })
+config.font = wezterm.font("JetbrainsMonoNL Nerd Font", { bold = true, italic = false })
 config.font_size = 12
 
 -- Window padding settings
 config.window_padding = {
-	left = 16,
-	right = 16,
-	top = 16,
-	bottom = 16,
+	left = 12,
+	right = 12,
+	top = 12,
+	bottom = 12,
 }
 
 -- Hide tab bar if only one tab is open
 -- config.hide_tab_bar_if_only_one_tab = true
 config.use_fancy_tab_bar = false
 config.tab_and_split_indices_are_zero_based = true
--- Disable tab bar
--- config.enable_tab_bar = false
 config.tab_bar_at_bottom = true
 
--- Set window background opacity
-config.window_background_opacity = 0.8
-
 -- Initial window size
-config.initial_rows = 32
+config.initial_rows = 40
 config.initial_cols = 120
+
+-- Linux specific settings
+
+config.window_decorations = "NONE"
+
+-- MacOS specific settings
+
+-- Enable macOS native fullscreen mode
+-- config.native_macos_fullscreen_mode = true
+
+-- config.window_decorations = "RESIZE"
+
+-- Key bindings
+-- config.keys = {
+-- 	{
+-- 		key = "w",
+-- 		mods = "CMD",
+-- 		action = wezterm.action.CloseCurrentTab({ confirm = false }),
+-- 	},
+-- 	{
+-- 		key = "f",
+-- 		mods = "CMD|CTRL",
+-- 		action = wezterm.action.ToggleFullScreen,
+-- 	},
+-- }
 
 -- Finally, return the configuration to wezterm
 return config
