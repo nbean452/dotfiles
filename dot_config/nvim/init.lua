509faa66d7579e60be4072172363b12a33d7605c
Nicholas Benedict <nicholasb1537@gmail.com>
2026-07-24 20:38:43 +0800

    feat(nvim): add toggle linewrap mapping

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -719,6 +719,8 @@ vim.keymap.set("n", "gd", vim.lsp.buf.definition)
 vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action)
 vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename)
 
+vim.keymap.set("n", "<leader>lw", "<CMD>set wrap!<CR>", { desc = "Toggle line wrap" })
+
 vim.keymap.set("n", "gr", function()
     vim.lsp.buf.references()
 end)
