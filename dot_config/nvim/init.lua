d51fa5cc74af99eabeb33ba4ddfe28e03f1bc7ec
Nicholas Benedict <nicholasb1537@gmail.com>
2026-07-24 20:38:23 +0800

    chore(nvim): add oil.nvim's default config

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -369,27 +369,129 @@ refresh.callback = function(...)
 end
 
 require("oil").setup({
+    -- Oil will take over directory buffers (e.g. `vim .` or `:e src/`)
+    -- Set to false if you want some other plugin (e.g. netrw) to open when you edit directories.
+    default_file_explorer = true,
+    -- Id is automatically added at the beginning, and name at the end
+    -- See :help oil-columns
+    columns = {
+        "icon",
+        -- "permissions",
+        -- "size",
+        -- "mtime",
+    },
+    -- Buffer-local options to use for oil buffers
+    buf_options = {
+        buflisted = false,
+        bufhidden = "hide",
+    },
+    -- Window-local options to use for oil buffers
     win_options = {
         signcolumn = "yes:2",
+        wrap = false,
+        cursorcolumn = false,
+        foldcolumn = "0",
+        spell = false,
+        list = false,
+        conceallevel = 3,
+        concealcursor = "nvic",
     },
+    -- Send deleted files to the trash instead of permanently deleting them (:help oil-trash)
+    delete_to_trash = false,
+    -- Skip the confirmation popup for simple operations (:help oil.skip_confirm_for_simple_edits)
+    skip_confirm_for_simple_edits = false,
+    -- Selecting a new/moved/renamed file or directory will prompt you to save changes first
+    -- (:help prompt_save_on_select_new_entry)
+    prompt_save_on_select_new_entry = true,
+    -- Oil will automatically delete hidden buffers after this delay
+    -- You can set the delay to false to disable cleanup entirely
+    -- Note that the cleanup process only starts when none of the oil buffers are currently displayed
+    cleanup_delay_ms = 2000,
+    lsp_file_methods = {
+        -- Enable or disable LSP file operations
+        enabled = true,
+        -- Time to wait for LSP file operations to complete before skipping
+        timeout_ms = 1000,
+        -- Set to true to autosave buffers that are updated with LSP willRenameFiles
+        -- Set to "unmodified" to only save unmodified buffers
+        autosave_changes = false,
+    },
+    -- Constrain the cursor to the editable parts of the oil buffer
+    -- Set to `false` to disable, or "name" to keep it on the file names
+    constrain_cursor = "editable",
+    -- Set to true to watch the filesystem for changes and reload oil
+    watch_for_changes = false,
+    -- Keymaps in oil buffer. Can be any value that `vim.keymap.set` accepts OR a table of keymap
+    -- options with a `callback` (e.g. { callback = function() ... end, desc = "", mode = "n" })
+    -- Additionally, if it is a string that matches "actions.<name>",
+    -- it will use the mapping at require("oil.actions").<name>
+    -- Set to `false` to remove a keymap
+    -- See :help oil-actions for a list of all available actions
+    keymaps = {
+        ["g?"] = { "actions.show_help", mode = "n" },
+        ["<CR>"] = "actions.select",
+        ["<C-s>"] = { "actions.select", opts = { vertical = true } },
+        ["<C-v>"] = { "actions.select", opts = { horizontal = true } },
+        ["<C-t>"] = { "actions.select", opts = { tab = true } },
+        ["<C-p>"] = "actions.preview",
+        ["<C-c>"] = { "actions.close", mode = "n" },
+        ["<C-l>"] = "actions.refresh",
+        ["-"] = { "actions.parent", mode = "n" },
+        ["_"] = { "actions.open_cwd", mode = "n" },
+        ["`"] = { "actions.cd", mode = "n" },
+        ["g~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
+        ["gs"] = { "actions.change_sort", mode = "n" },
+        ["gx"] = "actions.open_external",
+        ["g."] = { "actions.toggle_hidden", mode = "n" },
+        ["g\\"] = { "actions.toggle_trash", mode = "n" },
+    },
+    -- Set to false to disable all of the above keymaps
+    use_default_keymaps = true,
     view_options = {
+        -- Show files and directories that start with "."
+        show_hidden = false,
+        -- This function defines what is considered a "hidden" file
         is_hidden_file = function(name, bufnr)
-            local dir = require("oil").get_current_dir(bufnr)
-            local is_dotfile = vim.startswith(name, ".") and name ~= ".."
-            -- if no local directory (e.g. for ssh connections), just hide dotfiles
-            if not dir then
-                return is_dotfile
-            end
-            -- dotfiles are considered hidden unless tracked
-            if is_dotfile then
-                return not git_status[dir].tracked[name]
-            else
-                -- Check if file is gitignored
-                return git_status[dir].ignored[name]
-            end
+            local m = name:match("^%.")
+            return m ~= nil
+        end,
+        -- This function defines what will never be shown, even when `show_hidden` is set
+        is_always_hidden = function(name, bufnr)
+            return false
+        end,
+        -- Sort file names with numbers in a more intuitive order for humans.
+        -- Can be "fast", true, or false. "fast" will turn it off for large directories.
+        natural_order = "fast",
+        -- Sort file and directory names case insensitive
+        case_insensitive = false,
+        sort = {
+            -- sort order can be "asc" or "desc"
+            -- see :help oil-columns to see which columns are sortable
+            { "type", "asc" },
+            { "name", "asc" },
+        },
+        -- Customize the highlight group for the file name
+        highlight_filename = function(entry, is_hidden, is_link_target, is_link_orphan)
+            return nil
         end,
     },
+    -- Extra arguments to pass to SCP when moving/copying files over SSH
+    extra_scp_args = {},
+    -- Configuration for the file preview window
+    preview_win = {
+        -- Whether the preview window is automatically updated when the cursor is moved
+        update_on_cursor_moved = true,
+        -- How to open the preview window "load"|"scratch"|"fast_scratch"
+        preview_method = "fast_scratch",
+        -- A function that returns true to disable preview on a file e.g. to avoid lag
+        disable_preview = function(filename)
+            return false
+        end,
+        -- Window-local options to use for preview window buffers
+        win_options = {},
+    },
 })
+
 require("oil-git-status").setup({})
 require("line-numbers").setup({})
 require("text-transform").setup({
