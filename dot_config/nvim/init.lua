63666a9414339ee289f768031331f82788fd15e6
Nicholas Benedict <nicholasb1537@gmail.com>
2026-07-24 20:38:54 +0800

    refactor(nvim): not so eager linting
    Stopped linting from happening when cursor is moved, it's too eager and
    I personally have perf concerns on running linter with each movement :)

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -965,18 +965,23 @@ end
 
 setup_treesitter()
 
--- trigger linting
-vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "CursorMoved", "InsertLeave" }, {
-    callback = function()
-        -- try_lint without arguments runs the linters defined in `linters_by_ft`
-        -- for the current filetype
-        require("lint").try_lint()
+local trigger_lint = function()
+    vim.notify("Attempting to lint buffer...")
+    -- try_lint without arguments runs the linters defined in `linters_by_ft`
+    -- for the current filetype
+    require("lint").try_lint()
 
-        -- You can call `try_lint` with a linter name or a list of names to always
-        -- run specific linters, independent of the `linters_by_ft` configuration
-        require("lint").try_lint("codespell")
-    end,
+    -- You can call `try_lint` with a linter name or a list of names to always
+    -- run specific linters, independent of the `linters_by_ft` configuration
+    require("lint").try_lint("codespell")
+end
+
+-- trigger linting
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+    callback = trigger_lint,
 })
+
+vim.keymap.set("n", "<leader>ln", trigger_lint)
 
 vim.g.EasyMotion_do_mapping = 0
 vim.g.EasyMotion_smartcase = 1
