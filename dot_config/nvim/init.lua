98909fe8bc8f5fd7956383953e213c983255af4d
Nicholas Benedict <nicholasb1537@gmail.com>
2026-08-08 17:36:21 +0800

    chore(nvim): trigger linting on code format

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -655,9 +655,21 @@ vim.lsp.enable({
     "dartls", -- found in :help lspconfig-all
 })
 
+local trigger_lint = function()
+    vim.notify("Attempting to lint buffer...")
+    -- try_lint without arguments runs the linters defined in `linters_by_ft`
+    -- for the current filetype
+    require("lint").try_lint()
+
+    -- You can call `try_lint` with a linter name or a list of names to always
+    -- run specific linters, independent of the `linters_by_ft` configuration
+    require("lint").try_lint("codespell")
+end
+
 -- buffer format
 vim.keymap.set("n", "<leader>lf", function()
     require("conform").format()
+    trigger_lint()
 end)
 
 -- new and close buffers
@@ -965,21 +977,10 @@ end
 
 setup_treesitter()
 
-local trigger_lint = function()
-    vim.notify("Attempting to lint buffer...")
-    -- try_lint without arguments runs the linters defined in `linters_by_ft`
-    -- for the current filetype
-    require("lint").try_lint()
-
-    -- You can call `try_lint` with a linter name or a list of names to always
-    -- run specific linters, independent of the `linters_by_ft` configuration
-    require("lint").try_lint("codespell")
-end
-
 -- trigger linting
-vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-    callback = trigger_lint,
-})
+-- vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+--     callback = trigger_lint,
+-- })
 
 vim.keymap.set("n", "<leader>ln", trigger_lint)
 
